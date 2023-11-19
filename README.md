17739826ef4a6cb72e1d4034a53a3385b33bfaa1
dradoaica <dradoaica@gmail.com>
2023-11-19 17:49:46 +0200

    fix docker-entrypoint-unprivileged.sh

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # clamav-docker-openshift
 
 ## build clamav-docker-openshift
-docker build -t dradoaica/clamav-openshift:1.2.1 -f Dockerfile .
+docker build -t dradoaica/clamav-openshift:1.2.2 -f Dockerfile .
 
 ## push clamav-docker-openshift
-docker push dradoaica/clamav-openshift:1.2.1
+docker push dradoaica/clamav-openshift:1.2.2
 
 ## run container
-docker run -p 0.0.0.0:3310:3310/tcp --name clamav-openshift -d dradoaica/clamav-openshift:1.2.1
+docker run -p 0.0.0.0:3310:3310/tcp --name clamav-openshift -d dradoaica/clamav-openshift:1.2.2
 
 ## remove container
 docker rm -f clamav-openshift
 
 ## remove image
-docker rmi dradoaica/clamav-openshift:1.2.1
+docker rmi dradoaica/clamav-openshift:1.2.2
--- scripts/docker-entrypoint-unprivileged.sh
+++ scripts/docker-entrypoint-unprivileged.sh
@@ -1,24 +1,46 @@
-#!/bin/sh
+#!/sbin/tini /bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2021 Olliver Schinagl <oliver@schinagl.nl>
+# Copyright (C) 2021-2023 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
+#
+# A beginning user should be able to docker run image bash (or sh) without
+# needing to learn about --entrypoint
+# https://github.com/docker-library/official-images#consistency
+
 set -eu
+
 # run command if it is not starting with a "-" and is an executable in PATH
-if [ "${#}" -gt 0 ] && [ "${1#-}" = "${1}" ] && command -v "${1}" > "/dev/null" 2>&1; then
+if [ "${#}" -gt 0 ] && \
+   [ "${1#-}" = "${1}" ] && \
+   command -v "${1}" > "/dev/null" 2>&1; then
 	# Ensure healthcheck always passes
 	CLAMAV_NO_CLAMD="true" exec "${@}"
 else
-	if [ "${#}" -ge 1 ] && [ "${1#-}" != "${1}" ]; then
+	if [ "${#}" -ge 1 ] && \
+	   [ "${1#-}" != "${1}" ]; then
 		# If an argument starts with "-" pass it to clamd specifically
 		exec clamd "${@}"
 	fi
 	# else default to running clamav's servers
+
 	# Ensure we have some virus data, otherwise clamd refuses to start
 	if [ ! -f "/var/lib/clamav/main.cvd" ]; then
 		echo "Updating initial database"
 		freshclam --foreground --stdout
 	fi
+
 	if [ "${CLAMAV_NO_FRESHCLAMD:-false}" != "true" ]; then
 		echo "Starting Freshclamd"
-		freshclam --checks="${FRESHCLAM_CHECKS:-1}" --daemon --foreground --stdout --user="clamav" &
+		freshclam \
+		          --checks="${FRESHCLAM_CHECKS:-1}" \
+		          --daemon \
+		          --foreground \
+		          --stdout \
+		          --user="clamav" \
+			  &
 	fi
+
 	if [ "${CLAMAV_NO_CLAMD:-false}" != "true" ]; then
 		echo "Starting ClamAV"
 		if [ -S "/tmp/clamd.sock" ]; then
@@ -31,17 +53,20 @@ else
 				echo "Failed to start clamd"
 				exit 1
 			fi
-			printf "\n%s" "Socket for clamd not found yet, retrying (${_timeout}/${CLAMD_STARTUP_TIMEOUT}) ..."
+			printf "\r%s" "Socket for clamd not found yet, retrying (${_timeout}/${CLAMD_STARTUP_TIMEOUT}) ..."
 			sleep 1
 			_timeout="$((_timeout + 1))"
 		done
 		echo "socket found, clamd started."
 	fi
+
 	if [ "${CLAMAV_NO_MILTERD:-true}" != "true" ]; then
 		echo "Starting clamav milterd"
 		clamav-milter &
 	fi
+
 	# Wait forever (or until canceled)
 	exec tail -f "/dev/null"
 fi
+
 exit 0
